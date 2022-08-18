1c9ccf8a33c89de45301e35a909ab1959d424713
GabbsM <gabbsmoreno.gm@gmail.com>
2022-08-18 21:50:10 +0200

    Primer Commit. Organizamos estructura de proyecto.

---
+++ src/main/java/com/example/generadorPersonajesVampire/GeneradorPersonajesVampireApplication.java
@@ -0,0 +1,13 @@
+package com.example.generadorPersonajesVampire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneradorPersonajesVampireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneradorPersonajesVampireApplication.class, args);
+	}
+
+}
